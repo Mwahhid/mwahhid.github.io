f1288f95e0571c873b94023adb7943ce56475935
Mwahhid <78094284+Mwahhid@users.noreply.github.com>
2024-02-09 03:02:23 -0600

    Added a loader which runs while the scene is loading.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import { Suspense } from 'react'
+import {Canvas} from '@react-three/fiber'
+import Loader from '../components/Loader'
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         popup
       </div> */}
@@ -7,8 +8,14 @@ import React from 'react'
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      
+      <Canvas 
+        className="w-full h-screen bg-transparent"
+        camera={{near: 0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader />}>
 
+        </Suspense>
+      </Canvas>
     </section>
   )
 }
