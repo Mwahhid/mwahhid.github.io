6cb9a46406cdc31342411bf1d444f3061c3a87af
Mwahhid <78094284+Mwahhid@users.noreply.github.com>
2024-02-09 02:34:42 -0600

    Done setting up project and coding navigation bar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        popup
+      </div> */}
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+      
+
+    </section>
+  )
+}
+
+export default Home
