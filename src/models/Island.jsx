9f15b5b89d025076f389c19577ada7688a72cc3e
Mwahhid <78094284+Mwahhid@users.noreply.github.com>
2024-02-11 23:57:59 -0600

    Added other models and lighting

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -13,11 +13,76 @@ import { a } from "@react-spring/three";
 
 import islandScene from "../assets/3d/island.glb";
 
-const Island = (props) => {
+const Island = ({ isRotating, setIsRotating, ...props}) => {
     const islandRef = useRef();
 
+    const { gl, viewport } = useThree();
     const { nodes, materials } = useGLTF(islandScene);
+    const lastX = useRef(0);
+    const rotationSpeed = useRef(0);
+    const dampingFactor = 0.95; // makes the rotation drag slowly slow down
     
+    const handlePointerDown = (e) => {
+        e.stopProgation();
+        e.preventDefault();
+        setIsRotating(true);
+
+        const clientX = event.touches ? 
+        event.touches[0].clientX : e.clientX;
+
+        lastX.current = clientX;
+    }
+
+    const handlePointerUp = (e) => {
+        e.stopProgation();
+        e.preventDefault();
+        setIsRotating(false);
+    
+        const clientX = event.touches ? 
+        event.touches[0].clientX : e.clientX;
+
+        const delta = (clientX - lastX.current) / viewport.width;
+
+        islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+        lastX.current = clientX;
+        rotationSpeed.current = delta * 0.01 * Math.PI;
+    }
+
+    const handlePointerMove = (e) => {
+        e.stopProgation();
+        e.preventDefault();
+
+        if (isRotating) handlePointerUp(e);
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowRight') {
+            if(!isRotating) setIsRotating(true);
+            islandRef.current.rotation.y -= 0.01 * Math.PI;
+        } else if (e.key === 'ArrowLeft') {
+            if(!isRotating) setIsRotating(true);
+            islandRef.current.rotation.y += 0.01 * Math.PI;
+        }
+    }
+
+    const handleKeyUp = (e) => {
+        if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+            setIsRotating(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('pointerdown', handlePointerDown);
+        document.addEventListener('pointerMove', handlePointerMove);
+        document.addEventListener('pointerUp', handlePointerUp);
+        return () => {
+            document.removeEventListener('pointerdown', handlePointerDown);
+            document.removeEventListener('pointerMove', handlePointerMove);
+            document.removeEventListener('pointerUp', handlePointerUp);
+        }
+
+    }, [gl, handlePointerDown, handlePointerMove, handlePointerUp])
+
     return (
         <a.group ref={islandRef} {...props}>
             <mesh
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
-import { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import {Canvas} from '@react-three/fiber'
 import Loader from '../components/Loader'
 
 import Island from '../models/Island'
+import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
+
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         popup
       </div> */}
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -22,20 +28,53 @@ const Home = () => {
     return [screenScale, screenPosition, rotation]
   }
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4]
+    }
+
+    return [screenScale, screenPosition]
+  }
+
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
       <Canvas 
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${isRotating ? 
+          'cursor-grabbing' : 'cursorgrab'}`}
         camera={{near: 0.1, far: 1000}}
       >
         <Suspense fallback={<Loader />}>
-          <ambientLight />
+          <directionalLight position={[1, 1, 1]} intensity={2}/>
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000"
+          intensity={1} />
+
+          <Sky />
+          
+          <Bird />
+
+          <Plane
+            isRotating={isRotating} 
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+          />
+
           <Island 
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
           />
         </Suspense>
       </Canvas>
