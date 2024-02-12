b9c021a3ba48218bd2c9630359a27eb7c98b9e5e
Mwahhid <78094284+Mwahhid@users.noreply.github.com>
2024-02-12 01:33:44 -0600

    Implemented drag and move feature in 3d space.

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -23,36 +23,36 @@ const Island = ({ isRotating, setIsRotating, ...props}) => {
     const dampingFactor = 0.95; // makes the rotation drag slowly slow down
     
     const handlePointerDown = (e) => {
-        e.stopProgation();
+        e.stopPropagation();
         e.preventDefault();
         setIsRotating(true);
 
-        const clientX = event.touches ? 
-        event.touches[0].clientX : e.clientX;
+        const clientX = e.touches ? 
+        e.touches[0].clientX : e.clientX;
 
         lastX.current = clientX;
     }
 
     const handlePointerUp = (e) => {
-        e.stopProgation();
+        e.stopPropagation();
         e.preventDefault();
         setIsRotating(false);
-    
-        const clientX = event.touches ? 
-        event.touches[0].clientX : e.clientX;
-
-        const delta = (clientX - lastX.current) / viewport.width;
-
-        islandRef.current.rotation.y += delta * 0.01 * Math.PI;
-        lastX.current = clientX;
-        rotationSpeed.current = delta * 0.01 * Math.PI;
     }
 
     const handlePointerMove = (e) => {
-        e.stopProgation();
+        e.stopPropagation();
         e.preventDefault();
 
-        if (isRotating) handlePointerUp(e);
+        if (isRotating) {
+            const clientX = e.touches ? 
+            e.touches[0].clientX : e.clientX;
+    
+            const delta = (clientX - lastX.current) / viewport.width;
+    
+            islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+            lastX.current = clientX;
+            rotationSpeed.current = delta * 0.01 * Math.PI;
+        }
     }
 
     const handleKeyDown = (e) => {
@@ -71,14 +71,64 @@ const Island = ({ isRotating, setIsRotating, ...props}) => {
         }
     }
 
+    useFrame(() => {
+        if(!isRotating) {
+            rotationSpeed.current *= dampingFactor;
+
+            if (Math.abs(rotationSpeed.current) < 0.001) {
+                rotationSpeed.current = 0;
+            }
+
+            islandRef.current.rotation.y += rotationSpeed.current;
+
+        } else {
+            const rotation = islandRef.current.rotation.y;
+
+            const normalizedRotation =
+                ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          //setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          //setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          //setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          //setCurrentStage(1);
+          break;
+        default:
+          //setCurrentStage(null);
+      }
+        }
+
+    })
+
     useEffect(() => {
-        document.addEventListener('pointerdown', handlePointerDown);
-        document.addEventListener('pointerMove', handlePointerMove);
-        document.addEventListener('pointerUp', handlePointerUp);
+        // To attach all the elements to the canvas
+        const canvas = gl.domElement;
+        
+        canvas.addEventListener("pointerdown", handlePointerDown);
+        canvas.addEventListener("pointerup", handlePointerUp);
+        canvas.addEventListener("pointermove", handlePointerMove);
+        canvas.addEventListener("touchstart", handlePointerDown);
+        canvas.addEventListener("touchmove", handlePointerMove);
+        canvas.addEventListener("touchend", handlePointerUp);
+        window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
         return () => {
-            document.removeEventListener('pointerdown', handlePointerDown);
-            document.removeEventListener('pointerMove', handlePointerMove);
-            document.removeEventListener('pointerUp', handlePointerUp);
+            canvas.removeEventListener("pointerdown", handlePointerDown);
+            canvas.removeEventListener("pointerup", handlePointerUp);
+            canvas.removeEventListener("pointermove", handlePointerMove);
+            canvas.removeEventListener("touchstart", handlePointerDown);
+            canvas.removeEventListener("touchmove", handlePointerMove);
+            canvas.removeEventListener("touchend", handlePointerUp);
+            window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("keyup", handleKeyUp);
         }
 
     }, [gl, handlePointerDown, handlePointerMove, handlePointerUp])
