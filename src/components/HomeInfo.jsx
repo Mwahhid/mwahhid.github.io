7e6577a0e81f5d560cbe6153e2d0087eb5d8e95e
Mwahhid <78094284+Mwahhid@users.noreply.github.com>
2024-02-18 02:29:22 -0600

    Done with the Home page

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+            {text}
+        </p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain"/>
+        </Link>
+    </div>
+)
 
 const renderContent = {
     1: (
@@ -10,21 +24,29 @@ const renderContent = {
         </h1>
     ),
     2: (
-        <h1>2</h1>
+        <InfoBox 
+            text="Senior at Rhodes College, set to graduate in May 2024"
+            link="/about"
+            btnText="Learn more"
+        />
     ),
     3: (
-        <h1>3</h1>
+        <InfoBox 
+            text="Worked on multiple projects over the years to learn different skills.
+            I've shared them right here."
+            link="/projects"
+            btnText="Visit my portfolio"
+        />
     ),
     4: (
-        <h1>4</h1>
+        <InfoBox 
+            text="Need a project done or looking for a developer? 
+            I'm just a few keystrokes away"
+            link="/contact"
+            btnText="Let's talk"
+        />
     )
 }
-
-const InfoBox = ({ text, link, btnText }) => (
-    <div className="info-box">
-        {text}
-    </div>
-)
 
 const HomeInfo = ({ currentStage }) => {
   return renderContent[currentStage] || null;
