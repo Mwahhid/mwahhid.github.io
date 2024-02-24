57f0750b933e22a61edb208ef9e44b20a7fd37de
Mwahhid <78094284+Mwahhid@users.noreply.github.com>
2024-02-23 22:34:00 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  base: "/My_3D_Portfolio/"
+  assetsInclude: ['**/*.glb']
 })
